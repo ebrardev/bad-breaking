ebeba8dd3c2741fb470794995c1604b6cb1179bd
Ebrar <16155052@mersin.edu.tr>
2021-06-24 19:43:58 +0300

    first commit

---
+++ src/components/ui/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../../img/logo.png";
+
+const Header = () => {
+    return (
+        <header classname="center">
+            <img src={logo} alt="" />
+            
+        </header>
+    );
+};
+
+export default Header;
